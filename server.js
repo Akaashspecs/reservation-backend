998947fed2a51b11532eebb018d5d163491827a8
Aakash Saxena <aakashsaxena@Aakashs-Mac-mini.local>
2025-01-04 04:59:15 +0530

    port fix

--- server.js
+++ server.js
@@ -23,5 +23,5 @@ const bookingsRoute = require("./routes/booking");
 app.use("/api/bookings", bookingsRoute);
 
 // Start Server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));

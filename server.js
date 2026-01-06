e043999d73c8994b88bd3d6fa21d44b60bf5ad52
Aakash Saxena <aakashsaxena@Aakashs-Mac-mini.local>
2026-01-06 23:36:00 +0530

    fix website

--- server.js
+++ server.js
@@ -6,15 +6,20 @@ require("dotenv").config();
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:5173", // Vite frontend
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true, // if using cookies / auth
+  })
+);
+
 app.use(express.json());
 
 // Database Connection
 mongoose
-  .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB connected"))
   .catch((err) => console.error(err));
 
@@ -23,5 +28,5 @@ const bookingsRoute = require("./routes/booking");
 app.use("/api/bookings", bookingsRoute);
 
 // Start Server
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
